e6a96e63a65c0f4f996ef9be12f93086cf133489
Leandro Galvan <leusgalvan@gmail.com>
2020-02-13 09:22:09 -0300

    Refactor var example into object and change some variable names

--- src/main/scala/com/example/freemonadtoy/Var.scala
+++ src/main/scala/com/example/freemonadtoy/Var.scala
@@ -4,76 +4,86 @@ import cats._
 import cats.free.Free
 import cats.free.Free.liftF
 
-object ADT {
-  sealed trait VarA[A]
-  case class Set[T](a: T) extends VarA[Unit]
-  case class Get[T]() extends VarA[Option[T]]
-}
+object VarExample {
 
-object Ops {
-  import ADT._
+  object ADT {
 
-  type Var[A] = Free[VarA, A]
+    sealed trait Var[A]
 
-  def set[T](a: T): Var[Unit] = liftF[VarA, Unit](Set(a))
-  def get[T]: Var[Option[T]] = liftF[VarA, Option[T]](Get[T]())
-}
+    case class Set[T](a: T) extends Var[Unit]
 
-object Compilers {
-  import ADT._
+    case class Get[T]() extends Var[Option[T]]
 
-  def simpleCompiler: VarA ~> Id = new (VarA ~> Id) {
-    var v: Option[Any] = None
+  }
 
-    override def apply[A](varA: VarA[A]): Id[A] = varA match {
-      case Set(a) =>
-        v = Some(a)
-        ()
-      case Get() => v.map(_.asInstanceOf[A])
+  object Ops {
+
+    import ADT._
+
+    def set[T](a: T): Free[Var, Unit] = liftF[Var, Unit](Set(a))
+
+    def get[T]: Free[Var, Option[T]] = liftF[Var, Option[T]](Get[T]())
+  }
+
+  object Interpreters {
+
+    import ADT._
+
+    def simpleInterpreter: Var ~> Id = new (Var ~> Id) {
+      var v: Option[Any] = None
+
+      override def apply[A](varA: Var[A]): Id[A] = varA match {
+        case Set(a) => v = Some(a)
+        case Get()  => v.map(_.asInstanceOf[A])
+      }
+    }
+
+    def historyInterpreter: Var ~> List = new (Var ~> List) {
+      var p = List.empty[Any]
+
+      override def apply[A](varA: Var[A]): List[A] = varA match {
+        case Set(a) =>
+          p = a :: p
+          List(())
+        case Get() =>
+          p.map(x => Option(x.asInstanceOf[A]))
+      }
     }
   }
 
-  def listCompiler: VarA ~> List = new (VarA ~> List) {
-    var p = List.empty[Any]
+  object Main {
+    import Ops._
+    import ADT._
+    import cats.syntax.apply._
+    import cats.instances.option._
+    import cats.instances.list._
 
-    override def apply[A](varA: VarA[A]): List[A] = varA match {
-      case Set(a) =>
-        p = a :: p
-        List(())
-      case Get() =>
-        p.map(x => Option(x.asInstanceOf[A]))
+    val program: Free[Var, Option[Int]] = for {
+      _ <- set(1)
+      optX <- get[Int]
+      _ <- set(2)
+      optY <- get[Int]
+    } yield {
+      (optX, optY).tupled.map { case (x, y) => x + y }
     }
-  }
-}
-object Main {
-  import Ops._
-  import cats.syntax.apply._
-  import cats.instances.option._
-  import cats.instances.list._
 
-  val program: Var[Option[Int]] = for {
-    _ <- set(1)
-    optX <- get[Int]
-    _ <- set(2)
-    optY <- get[Int]
-  } yield {
-    (optX, optY).tupled.map { case (x, y) => x + y }
-  }
+    val program2: Free[Var, Option[Int]] = for {
+      x <- get[Int]
+      _ <- set(42)
+    } yield x
 
-  val program2: Var[Option[Int]] = for {
-    x <- get[Int]
-    _ <- set(42)
-  } yield x
+    val programWithIdResults = program.foldMap(Interpreters.simpleInterpreter)
+    val programWithListResults =
+      program.foldMap(Interpreters.historyInterpreter)
+    val program2WithIdResults = program2.foldMap(Interpreters.simpleInterpreter)
+    val program2WithListResults =
+      program2.foldMap(Interpreters.historyInterpreter)
 
-  val programWithIdResults = program.foldMap(Compilers.simpleCompiler)
-  val programWithListResults = program.foldMap(Compilers.listCompiler)
-  val program2WithIdResults = program2.foldMap(Compilers.simpleCompiler)
-  val program2WithListResults = program2.foldMap(Compilers.listCompiler)
-
-  def run() = {
-    println(s"Program with Id: $programWithIdResults")
-    println(s"Program with List: $programWithListResults")
-    println(s"Program 2 with Id: $program2WithIdResults")
-    println(s"Program 2 with List: $program2WithListResults")
+    def run() = {
+      println(s"Program with Id: $programWithIdResults")
+      println(s"Program with List: $programWithListResults")
+      println(s"Program 2 with Id: $program2WithIdResults")
+      println(s"Program 2 with List: $program2WithListResults")
+    }
   }
 }
